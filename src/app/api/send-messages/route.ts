3210360056a9a5d0ae2e8fbe18451c0b3d064a3f
Preeth02 <preethampreetham27@gmail.com>
2025-02-18 17:36:49 +0530

    add a method to send messages to the user

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,54 @@
+import connectDB from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+  await connectDB();
+  try {
+    const { username, content } = await request.json();
+    const user = await UserModel.findOne(username);
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    } else if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting the message right now",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+    return Response.json(
+      {
+        success: true,
+        message: "Message has been sent to the user",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "Something went wrong while sending the message to the user",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
